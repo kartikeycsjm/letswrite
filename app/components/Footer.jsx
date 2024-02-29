cc2e32ef7f0124fad89ff26c89d9950d773efccf
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-03-01 00:04:17 +0530

    sourcecode added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div id="footer" className='w-full h-[80px] text-white bg-[#1b1a1a] flex justify-around items-center flex-col text-[10px]'>
             <p>&copy; Kartikey Mishra 2024</p>
-            <a className='text-blue-500 underline' href="/">source code</a>
+            <a className='text-blue-500 underline' href="https://github.com/kartikeycsjm/letswrite">source code</a>
         </div>
     )
 }
